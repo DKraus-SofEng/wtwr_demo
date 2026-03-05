6d558f5990a2043baee24a8b1480d00c8bd04667
DELIGHT KRAUS <dkraus.sofeng@gmail.com>
2026-03-04 17:12:52 -0700

    Updates for cloud deployment

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,5 +1,5 @@
 // Centralized API base URL for all API calls
 export const BASE_URL =
     process.env.NODE_ENV === "production"
-        ? "https://api.wtwr.bot.nu"
-        : "http://34.72.105.93:3001";
+        ? "https://api.wtwr.bot.nu" //TODO update after deployment
+        : "http://localhost:8080";
